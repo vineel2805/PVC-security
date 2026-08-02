8c90c439b99407444bd816607d445953942ea6b5
vineel <vineel2805@gmail.com>
2026-08-02 11:33:56 +0530

    refactor: clean up head.php by removing duplicate favicon links and consolidating CSS includes

--- head.php
+++ head.php
@@ -11,7 +11,11 @@
  
   <!--=====FAB ICON=======-->
   <link rel="shortcut icon" href="assets/img/logo/Untitled design-3.png" type="image/x-icon">
+  <link rel="shortcut icon" href="assets/img/logo/logo1.png" type="image/x-icon">
   <!--===== CSS LINK =======-->
+
+
+  <!-- CSS Plugins -->
    <link rel="stylesheet" href="assets/css/plugins/bootstrap.min.css">
    <link rel="stylesheet" href="assets/css/plugins/aos.css">
    <link rel="stylesheet" href="assets/css/plugins/fontawesome.css">
@@ -21,26 +25,22 @@
    <link rel="stylesheet" href="assets/css/plugins/sidebar.css">
    <link rel="stylesheet" href="assets/css/plugins/slick-slider.css">
    <link rel="stylesheet" href="assets/css/plugins/nice-select.css">
+
    <link rel="stylesheet" href="assets/css/main.css">
    <link rel="stylesheet" href="assets/css/header_styles.css">
-   <link rel="stylesheet" href="assets/css/cart_styles.css">
+   <link rel="stylesheet" href="assets/css/global_header.css">
+   <link rel ="stylesheet" href="assets/css/global_footer.css">
    <link rel="stylesheet" href="assets/css/products_styles.css">
    <link rel="stylesheet" href="assets/css/index_styles.css">
-    <link rel="stylesheet" href="assets/css/global_header.css">
-    <link rel ="stylesheet" href="assets/css/global_footer.css"> 
-    <link rel="shortcut icon" href="assets/img/logo/logo1.png" type="image/x-icon">
+    
+     
+    
     <link rel="stylesheet" href="assets/css/main.css?v=1785408043">
     <link rel="stylesheet" href="assets/css/header_styles.css?v=1785408043">
-    <link rel="stylesheet" href="assets/css/products_styles.css">
     <link rel="stylesheet" href="assets/css/all-products.css?v=1785408043">
-    <link rel="stylesheet" href="assets/css/cart_styles.css">
-    <link rel="stylesheet" href="assets/css/card-fix.css"> 
-    <link rel="stylesheet" href="assets/css/pvc-header-footer.css">
     <link rel="stylesheet" href="assets/css/cart_styles.css">   
 <!-- JS Plugins -->
   <script src="assets/js/plugins/jquery-3-6-0.min.js"></script>
   <script src="assets/js/plugins/waypoints.js"></script>
-  
- <!--<link rel="stylesheet" href="assets/css/index.css">-->
 </head>
 <body>
